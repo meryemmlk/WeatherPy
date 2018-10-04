14f9f30126dc56590a3fb143335c96a552e3225b
meryemmlk <meryemkaratas@yahoo.com>
2018-10-03 22:50:18 -0400

    Final files

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,4 @@
 # OpenWeatherMap API Key
-#api_key = "2ccebca6fe632416eb2c16621b575c63"
-api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
-# Enter your API key
+api_key = "2ccebca6fe632416eb2c16621b575c63"
+
+
