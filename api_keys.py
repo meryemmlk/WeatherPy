50b01682f85d36cf4e6d6983ea9a2721d72d122b
meryemmlk <meryemkaratas@yahoo.com>
2018-10-03 22:25:49 -0400

    Final homework files

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+#api_key = "2ccebca6fe632416eb2c16621b575c63"
+api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
+# Enter your API key
+gkey = "AIzaSyCT1kl3HMfU0oOvSt4vTgysiQW1LXXl4Bw"
